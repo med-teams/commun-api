1c1e1aa655819aee67bdf83cd7f4c4d1d551b4c6
Mohamed Elhanchir <elhanchirmohamed349@gmail.com>
2026-03-29 19:35:07 +0100

    fix 102

--- src/main/java/elhanchir/mohamed/ebanking/repositories/CustomerRepository.java
+++ src/main/java/elhanchir/mohamed/ebanking/repositories/CustomerRepository.java
@@ -4,8 +4,4 @@ import elhanchir.mohamed.ebanking.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CustomerRepository extends JpaRepository<Customer,Long> {
-    Customer countCustomerByEmail(String email);
-    Customer countAllByEmail(String email);
-    Customer findCustomerByEmail(String email);
-    Customer getAllByEmailContains(String email);
 }
