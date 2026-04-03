eb18e622feb11c0576a9bef8af5f2fb18e9d345b
Mohamed Elhanchir <elhanchirmohamed349@gmail.com>
2026-04-03 21:59:35 +0100

    fix 104

---
+++ src/main/java/elhanchir/mohamed/ebanking/Utils/Calculator.java
@@ -0,0 +1,14 @@
+package elhanchir.mohamed.ebanking.Utils;
+
+public final class Calculator {
+
+    private Calculator() {}
+
+    public static int additionner(int a, int b) {
+        return a + b;
+    }
+
+    public static int multiplier(int a, int b) {
+        return a * b;
+    }
+}
